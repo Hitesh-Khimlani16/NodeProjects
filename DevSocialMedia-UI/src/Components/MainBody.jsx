d038812ecec56e85873b70b617143096c26db58d
Hitesh-Khimlani16 <17ec.hitesh.khimlani@gmail.com>
2026-09-06 20:32:20 +0530

    Logout feature and remove unwanted commnets

--- DevSocialMedia-UI/src/Components/MainBody.jsx
+++ DevSocialMedia-UI/src/Components/MainBody.jsx
@@ -8,44 +8,6 @@ import { BASE_API_URL, LOGIN_END_POINT, PROFILE_END_POINT } from "../Utils/BASE_
 import { addUSer } from "../Store/userSlice";
 
 const MainBody = () => {
-  // const userData = useSelector(store=>store.user);
-  // console.log("userData = ", userData);
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
-
-  // const [loading, setLoading] = useState(true);
-
-  // const fetchUser = async () => {
-  //   if (userData) return;
-  //   try {
-  //     const res = await axios.get(
-  //       BASE_API_URL + PROFILE_END_POINT, 
-  //       { withCredentials: true }
-  //     );
-  //     console.log("response::", res?.data);
-  //     if(res?.data){
-  //       dispatch(addUSer(res.data));
-  //     }
-  //   } catch (error) {
-  //     console.log("Something went wrong...", error);
-  //     navigate(LOGIN_END_POINT);
-  //   } finally{
-  //     setLoading(false);
-  //   }
-  // }
-
-  // useEffect(()=>{
-  //   // if(!userData){
-  //   //   setLoading(false);
-  //   //   return;
-  //   // }
-  //   fetchUser();
-  // }, []);
-
-  // if(loading) {
-  //   return <h1>Loading...</h1>
-  // }
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
